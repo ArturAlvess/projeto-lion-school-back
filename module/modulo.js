f0fa9ced9f04fe39d5aa9c007b9a7e53e6937d05
ArturAlvess <contatopessoal997@gmail.com>
2023-04-09 00:56:59 -0300

    testes back end

--- module/modulo.js
+++ module/modulo.js
@@ -79,9 +79,7 @@ const alunos = require('../json/alunos.js');
          listaDadosAlunoJSON.matricula = lista.matricula;
          listaDadosAlunoJSON.sexo = lista.sexo,
          listaDadosAlunoJSON.status = lista.status,
-         listaDadosAlunoJSON.curso = lista.curso[0].nome,
-         listaDadosAlunoJSON.dataConclusao = lista.curso[0].conclusao
-
+         
          informacoes.push(listaDadosAlunoJSON)
 
        }

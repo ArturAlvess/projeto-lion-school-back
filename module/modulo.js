ae0e277aac3a5ff58ffb464d3a3aea6119015529
Artur Alves <22282175@CFP127ALUNO.LOCAL>
2023-04-13 16:05:33 -0300

    mudancas back-end

--- module/modulo.js
+++ module/modulo.js
@@ -5,114 +5,141 @@
  * Version 1.0
  ****/
 
- var listaCursos = require('../json/cursos.js')
- var listaAlunos = require('../json/alunos.js');
+var listaCursos = require('../json/cursos.js')
+var listaAlunos = require('../json/alunos.js');
 const alunos = require('../json/alunos.js');
 
- const getListaCursos = function(){
-    return listaCursos;
- }
+const getListaCursos = function () {
+   return listaCursos;
+}
 
- const getListaAlunos = function(){
+const getListaAlunos = function () {
 
-    let listaDadosAlunoJSON = false;
-    let informacoes = []
-    let alunos = {}
+   let listaDadosAlunoJSON = false;
+   let informacoes = []
+   let alunos = {}
 
-    listaAlunos.alunos.forEach(function(aluno){
-        listaDadosAlunoJSON = {}
+   listaAlunos.alunos.forEach(function (aluno) {
+      listaDadosAlunoJSON = {}
 
-        
-        listaDadosAlunoJSON.nome = aluno.nome;
-        listaDadosAlunoJSON.foto = aluno.foto;
-        listaDadosAlunoJSON.matricula = aluno.matricula;
-        listaDadosAlunoJSON.sexo = aluno.sexo;
 
-        informacoes.push(listaDadosAlunoJSON)
+      listaDadosAlunoJSON.nome = aluno.nome;
+      listaDadosAlunoJSON.foto = aluno.foto;
+      listaDadosAlunoJSON.matricula = aluno.matricula;
+      listaDadosAlunoJSON.sexo = aluno.sexo;
 
-    })
+      informacoes.push(listaDadosAlunoJSON)
 
-    alunos = {
-       informacoes
-    }
-    return alunos
+   })
 
- }
+   alunos = {
+      informacoes
+   }
+   return alunos
 
- const getAlunoMatricula = function(matricula){
+}
+
+const getSigla = function(palavraNaoAbreviada){
+   let materias = palavraNaoAbreviada.replace(/\s(de|da|a|das)\s/g, ' ');
+   materias = materias.split(' ');
+   const initials = materias.map(materia => materia.charAt(0).toUpperCase());
+   return initials.join('');
+
+} 
+const getAlunoMatricula = function (matricula) {
 
    let alunoMatricula = matricula
-   let listaDadosAlunoJSON = false;
+   let listaInformacoesJson = {}
+   let informacoesDisciplinasArray = []
+   let informacoesDisciplinasJson
 
-   listaAlunos.alunos.forEach(function(aluno){
-      if(aluno.matricula == alunoMatricula){
-         listaDadosAlunoJSON = {}
+   listaAlunos.alunos.forEach(function (lista) {
+      if (alunoMatricula == lista.matricula) {
+         listaInformacoesJson = {
 
-         
-         listaDadosAlunoJSON.nome = aluno.nome;
-         listaDadosAlunoJSON.foto = aluno.foto;
-         listaDadosAlunoJSON.matricula = aluno.matricula;
-         listaDadosAlunoJSON.sexo = aluno.sexo;
+            nome: lista.nome,
+            foto: lista.foto,
+            matricula: lista.matricula,
+            sexo: lista.sexo,
+            status: lista.status,
+            nomeCurso: lista.curso[0].nome,
+            sigla: lista.curso[0].sigla,
+            icone: lista.curso[0].icone,
+            conclusao: lista.curso[0].conclusao
+         }
+
+         lista.curso[0].disciplinas.forEach(function(listaDisciplina){
+            listaInformacoesJson.disciplinas = informacoesDisciplinasArray
+
+            informacoesDisciplinasJson = {
+               nomeDisciplina: listaDisciplina.nome,
+               sigla: getSigla(listaDisciplina.nome),
+               media: listaDisciplina.media,
+               status: listaDisciplina.status
+            }
+            informacoesDisciplinasArray.push(informacoesDisciplinasJson)
+         })
+
+
 
       }
    })
 
-   return listaDadosAlunoJSON;
- 
- }
+   return listaInformacoesJson;
 
- const getAlunosCurso = function(cursoSigla){
+}
+
+const getAlunosCurso = function (cursoSigla) {
 
    listaDadosAlunoJSON = false;
-    let siglaCurso = cursoSigla;
-    let alunos = {}
-    let informacoes = []
+   let siglaCurso = cursoSigla;
+   let alunos = {}
+   let informacoes = []
 
-    listaAlunos.alunos.forEach(function(lista){
-       if(siglaCurso.toUpperCase() == lista.curso[0].sigla.toUpperCase()){
+   listaAlunos.alunos.forEach(function (lista) {
+      if (siglaCurso.toUpperCase() == lista.curso[0].sigla.toUpperCase()) {
 
          listaDadosAlunoJSON = {}
 
-         
+
          listaDadosAlunoJSON.nome = lista.nome;
          listaDadosAlunoJSON.foto = lista.foto;
          listaDadosAlunoJSON.matricula = lista.matricula;
          listaDadosAlunoJSON.sexo = lista.sexo,
-         listaDadosAlunoJSON.status = lista.status,
-         listaDadosAlunoJSON.curso = lista.curso[0].nome,
-         listaDadosAlunoJSON.dataConclusao = lista.curso[0].conclusao
+            listaDadosAlunoJSON.status = lista.status,
+            listaDadosAlunoJSON.curso = lista.curso[0].nome,
+            listaDadosAlunoJSON.dataConclusao = lista.curso[0].conclusao
 
          informacoes.push(listaDadosAlunoJSON)
 
-       }
-    })
-    alunos = {
-       informacoes
-    }
-    return alunos
+      }
+   })
+   alunos = {
+      informacoes
+   }
+   return alunos
 
- }
-console.log(getAlunosCurso('DS'))
- const getStatusAluno = function(status){
+}
+const getStatusAluno = function (status) {
 
    listaDadosAlunoJSON = false;
    let statusAluno = status;
    let alunos = {}
    let informacoes = []
 
-   listaAlunos.alunos.forEach(function(lista){
-      if(statusAluno.toUpperCase() == lista.status.toUpperCase()){
+   listaAlunos.alunos.forEach(function (lista) {
+      if (statusAluno.toUpperCase() == lista.status.toUpperCase()) {
 
          listaDadosAlunoJSON = {}
 
-         
+
          listaDadosAlunoJSON.nome = lista.nome;
          listaDadosAlunoJSON.foto = lista.foto;
          listaDadosAlunoJSON.matricula = lista.matricula;
          listaDadosAlunoJSON.sexo = lista.sexo;
          listaDadosAlunoJSON.status = lista.status,
-         listaDadosAlunoJSON.curso = lista.curso[0].nome,
-         listaDadosAlunoJSON.dataConclusao = lista.curso[0].conclusao;
+            listaDadosAlunoJSON.curso = lista.curso[0].nome,
+            listaDadosAlunoJSON.dataConclusao = lista.curso[0].conclusao;
 
          informacoes.push(listaDadosAlunoJSON)
 
@@ -123,12 +150,12 @@ console.log(getAlunosCurso('DS'))
       informacoes
    }
    return alunos
- }
+}
 
- module.exports = {
-    getListaCursos,
-    getListaAlunos,
-    getAlunoMatricula,
-    getAlunosCurso,
-    getStatusAluno
+module.exports = {
+   getListaCursos,
+   getListaAlunos,
+   getAlunoMatricula,
+   getAlunosCurso,
+   getStatusAluno
 }
